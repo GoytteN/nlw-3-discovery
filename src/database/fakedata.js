026646461254daaad923fdd9e673cd1d9d9afecf
João Pedro <joaodev84@gmail.com>
2020-10-15 15:03:40 -0300

    second commit - Aula quatro

---
+++ src/database/fakedata.js
@@ -0,0 +1,53 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-19.9023386",
+    lng:"-44.1041357",
+    name: "Lar das meninas",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio dolore mollitia ratione fugiat magni sequi corporis enim, perspiciatis deserunt quis tempore aliquid saepe, deleniti voluptate eos voluptatum praesentium recusandae quia!",
+
+    images: [
+      "https://images.unsplash.com/photo-1600711724564-526eda91ac29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1562790519-76a1627de754?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horários de visitas das 18h até 8h",
+    open_on_weekends: "1"
+  },
+
+  {
+    id: 2,
+    lat: "-19.9023386",
+    lng:"-44.1141357",
+    name: "Lar dos meninos",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio dolore mollitia ratione fugiat magni sequi corporis enim, perspiciatis deserunt quis tempore aliquid saepe, deleniti voluptate eos voluptatum praesentium recusandae quia!",
+
+    images: [
+      "https://images.unsplash.com/photo-1600711724564-526eda91ac29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1562790519-76a1627de754?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horários de visitas das 5h até 18h",
+    open_on_weekends: "0"
+  },
+
+  
+  {
+    id: 3,
+    lat: "-19.9123386",
+    lng:"-44.1141357",
+    name: "Lar dos meninos",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio dolore mollitia ratione fugiat magni sequi corporis enim, perspiciatis deserunt quis tempore aliquid saepe, deleniti voluptate eos voluptatum praesentium recusandae quia!",
+
+    images: [
+      "https://images.unsplash.com/photo-1600711724564-526eda91ac29?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1562790519-76a1627de754?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horários de visitas das 5h até 18h",
+    open_on_weekends: "0"
+  }
+]
